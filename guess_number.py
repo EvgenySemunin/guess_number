8344b9fa626626839fed69860481ba6c71ae73fa
Evgeny Semunin <jonsam@mail.ru>
2023-12-27 16:55:51 +0300

    guess_num add elif

--- guess_number.py
+++ guess_number.py
@@ -1,12 +1,13 @@
 from random import randint
 number = randint(1, 100)
+print(number)
 print('Guess number')
 while True:
-    guess = int(input('Введите число'))
+    guess = int(input('Введите число - 3'))
     if guess < number:
         print('Ваше число меньше того, что загадано.')
-    if guess > number:
+    elif guess > number:
         print('Ваше число больше того, что загадано. ')
-    if guess == number:
+    elif guess == number:
         break
 print('Отличная интуиция! Вы угадали число :)')
